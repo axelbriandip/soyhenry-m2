b3a8434424654a4b3a92f2f850860ad26aa15cda
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-08-08 04:48:05 -0300

    bundle.js added

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,11 +1,6 @@
 const renderCards = require('./renderCards')
 
-const makeRequest = url => {
-    $.get(url, (data, status) => {
-        return data
-    });
-}
-
 const urlAPI = "https://students-api.2.us-1.fl0.io/movies";
-renderCards(makeRequest(urlAPI))
-// console.log(renderCards(makeRequest(url)));
+$.get(urlAPI, (data, status) => {
+    renderCards(data)
+});
