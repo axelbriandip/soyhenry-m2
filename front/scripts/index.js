a78d39db4871db038edecb244485268418fe4b43
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-08-12 23:14:10 -0300

    init scaffolding back done

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,8 @@
 const renderCards = require('./renderCards')
 
 const urlAPI = "https://students-api.2.us-1.fl0.io/movies";
+
 $.get(urlAPI, (data, status) => {
-    renderCards(data)
+    // renderCards(data)
+    console.log(data);
 });
